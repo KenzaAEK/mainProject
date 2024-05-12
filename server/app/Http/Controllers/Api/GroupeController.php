e22326667c67cd6e29a4f5e2d4e41666437c1040
Mehdi_EL_Adnani <eladnanimehdi03@gmail.com>
2024-05-12 23:24:52 +0100

    Avant Pagination

--- server/app/Http/Controllers/Api/GroupeController.php
+++ server/app/Http/Controllers/Api/GroupeController.php
@@ -14,6 +14,7 @@ class GroupeController extends Controller
      */
      
      public function index() {
+       
         $animateurs = Animateur::with(['user', 'groupes.enfants', 'horaires'])->get();
     
         $resultats = $animateurs->map(function ($animateur) {
