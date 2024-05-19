4f2b1ab75da0030edc3708f57aa2b57e49c667fe
Mehdi_EL_Adnani <eladnanimehdi03@gmail.com>
2024-05-19 15:13:01 +0100

    probleme regler

--- server/routes/api.php
+++ server/routes/api.php
@@ -1,62 +1,48 @@
 <?php
 
+use App\Http\Controllers\AdministrateurController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\Api\ActiviteController;
-<<<<<<< HEAD
 use App\Http\Controllers\Api\OffreController;
-=======
-use App\Http\Controllers\Api\AdministrateurController;
->>>>>>> 6046743ad57e3297c7247fd5ebe041feac53d267
 use App\Http\Controllers\Api\DemandeInscriptionController;
-use App\Http\Controllers\Api\DevisController;
 use App\Http\Controllers\Api\EnfantController;
-<<<<<<< HEAD
 use App\Http\Controllers\DevisController;
 use App\Http\Controllers\Api\GroupeController;
-=======
-use App\Http\Controllers\Api\Password\PasswordResetController;
-use App\Http\Controllers\Api\TypeActiviteController;
-
-/*
-╔==========================================================================╗
-║                           All Users Routes                                   ║
-╚==========================================================================╝
-*/
-
-Route::post('/password/email', [PasswordResetController::class, 'sendResetLinkEmail']);
-Route::post('/password/reset', [PasswordResetController::class, 'reset']);
-
-Route::post('/forgot-password', [PasswordResetController::class, 'forgotPassword']);
-Route::post('/reset-password/{token}', [PasswordResetController::class, 'resetPassword'])->name('password.reset');
-
-
-
-
-
-
-
-
->>>>>>> 6046743ad57e3297c7247fd5ebe041feac53d267
+use App\Http\Controllers\AnimateurController;
 
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
 
 
 
+Route::apiResource('enfants', EnfantController::class);
+Route::apiResource('demande-Inscriptions', DemandeInscriptionController ::class);
+
+Route::post('/devis/{id}/accept', [DevisController::class, 'acceptDevis']);
+Route::post('/devis/{id}/reject', [DevisController::class, 'rejectDevis']);
+
+
+
+
+Route::post('/admins/approve-demande/{id}', [AdministrateurController::class, 'approveDemande']);
+Route::post('/admin/reject-demande/{id}', [AdministrateurController::class, 'rejectDemande']);
+
+
 Route::group(['middleware' => 'auth:sanctum'], function () {
     // for authenticated users
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::get('/refresh', [AuthController::class, 'refreshToken']);
-<<<<<<< HEAD
     Route::apiResource('activites', ActiviteController::class);
     Route::get('/ateliers',[ActiviteController::class ,'getAtelier' ]);// cette methode sera tres utile pour recuperer les atelier present !!![il faut appeler cette api en premier ] pour le store 
     
    //********Traitement d'Animateurs*************** 
    
-   
-   
+   Route::get('/Animateurs',[AnimateurController::class,'AffAnimConnecter']);// Afficher ici les informations de l'Animateur connecter
+   Route::get('/AnimateursEnf',[AnimateurController::class,'AffEtudAnim']);
+   Route::get('/search_students',[AnimateurController::class,'searshEtud']);
+
    
     // ********Traitement Administrateurs**********
     
@@ -66,8 +52,6 @@ Route::group(['middleware' => 'auth:sanctum'], function () {
     Route::put('/offres/{offres}',[OffreController::class,'update']);
     Route::delete('/offres/{offres}/{activites}',[OffreController::class,'deleteOffreActiviteById']);// suppr une activite lier a une offre 
     Route::delete('/offres/{offres}',[OffreController::class,'deleteOffreActivitesByIdOffre']);// supprimer l'offre et tous  ces activites 
-   
-   
     Route::get('/animateurs', [GroupeController::class, 'index']);
 
 
@@ -83,95 +67,17 @@ Route::group(['middleware' => 'auth:sanctum'], function () {
 
     //add middlewear check role 
     // for parents only and authenticated
-    Route::apiResource('enfants', EnfantController::class);
-    Route::apiResource('demande-Inscriptions', DemandeInscriptionController ::class); 
-    Route::post('/devis/{id}/accept', [DevisController::class, 'acceptDevis']);
-    Route::post('/devis/{id}/reject', [App\Http\Controllers\DevisController::class, 'rejectDevis']);
+  
 
 
 
     //add middlewear check role 
     // for animators only and authenticated 
-=======
-    // Route::post('/upload-image', [ProfileController::class, 'uploadImage']);
-    // Route::post('/profile', [ProfileController::class, 'profile']);
-    // Route::post('/udpdate-profile', [ProfileController::class, 'updateProfile']); gate for animateur**** email 
-    // Route::post('/password/update', [ UpdatePasswordController::class, 'UpdatePassword']);
-    //Route::post('/password/reset', [ResetController::class, 'ResetPassword']); *********
-
-    // Manage notifications
-    // Route::get('/notifications', [NotificationController::class, 'index']);
-    // Route::get('/notifications/{notification}', [NotificationController::class, 'show']);
-    // Route::put('/notifications/{notification}/mark-as-read', [NotificationController::class, 'markAsRead']);
-    // Route::put('/notifications/{notification}/mark-as-unread', [NotificationController::class, 'markAsUnread']);
-    // Route::delete('/notifications/{notification}', [NotificationController::class, 'destroy']);
-    // Route::put('/notifications/mark-all-as-read', [NotificationController::class, 'markAllAsread']);
-    // Route::put('/notifications/mark-all-as-unread', [NotificationController::class, 'markAllAsUnread']);
-
-
-
-/*
-╔==========================================================================╗
-║                           Admin Routes                                   ║
-╚==========================================================================╝
-*/
-    Route::group(['middleware' => 'role:2', 'prefix' => 'admin'], function () {
-  
-        Route::apiResource('activites', ActiviteController::class);
-        Route::apiResource('type-activites', TypeActiviteController::class);
-        Route::post('/admins/approve-demande/{id}', [AdministrateurController::class, 'approveDemande']);
-        Route::post('/admin/reject-demande/{id}', [AdministrateurController::class, 'rejectDemande']);
-        // Route::post('/offres',[OffreController::class,'store']);
-        // Route::get('/offres/{offres}',[OffreController::class,'show']);
-        // Route::put('/offres/{offres}',[OffreController::class,'customUpdate']);
-        // Route::post('/offres/{offres}/{activites}',[OffreController::class,'destroy']);
-        // Route::get('/animateurs', [GroupeController::class, 'index']);
-    });
-
-
-
-
-/*
-╔==========================================================================╗
-║                           Parent Routes                                  ║
-╚==========================================================================╝
-*/
-    Route::group([ 'prefix' => 'parent'], function () {
-
-
-
-        Route::apiResource('enfants', EnfantController::class);
-        Route::apiResource('demande-Inscriptions', DemandeInscriptionController ::class); 
-        Route::post('/devis/{id}/accept', [DevisController::class, 'acceptDevis']);
-        Route::post('/devis/{id}/reject', [DevisController::class, 'rejectDevis']);
-        
-        // Route::get('parent/offres', [OffreController::class, 'index']);
-        // Route::get('parent/offres/{offre}', [OffreController::class, 'show']);
-        // Route::get('parent/offres/{offre}/details', [OffreController::class, 'showDetails']);
-    });    
-
-
-    
->>>>>>> 6046743ad57e3297c7247fd5ebe041feac53d267
     
     
-/*
-╔==========================================================================╗
-║                           Animateur Routes                              ║
-╚==========================================================================╝
-*/
-    Route::group([ 'prefix' => 'animateur'], function () {
-        
-    
-    
-    
-    
-    });    
+
 
 
 
 
 });
-
-
-
