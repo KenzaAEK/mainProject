7f7df23a86b6028f5c79532c157081cd8a573ffa
Mehdi_EL_Adnani <eladnanimehdi03@gmail.com>
2024-06-11 12:55:53 +0100

    la demande d'inscription sa marche tester avec postman

--- server/app/Http/Controllers/Api/DemandeInscriptionController.php
+++ server/app/Http/Controllers/Api/DemandeInscriptionController.php
@@ -202,146 +202,145 @@ class DemandeInscriptionController extends Controller
 
 
 
-   public function store(Request $request)
-{
-    DB::beginTransaction();
-    try {
-        $dmInscription = new DemandeInscription();
-        $dmInscription->optionsPaiement = 'mois';
-        $user = $request->user();
-        dd($user);
-        $idTuteur = 3;
-        $Secenfants = $request->enfants;
-        $nbrEnfants = is_array($Secenfants) ? count($Secenfants) : 0;
-        $dmInscription->idTuteur = $idTuteur;
+     public function store(Request $request)
+    {
+        DB::beginTransaction();
+        try {
+            $dmInscription = new DemandeInscription();
+            $dmInscription->optionsPaiement = 'mois';
+            $user = $request->user();
+            $idTuteur = 3;
+            $Secenfants = $request->enfants;
+            $nbrEnfants = is_array($Secenfants) ? count($Secenfants) : 0;
+            
+            $dmInscription->idTuteur = $idTuteur;
 
-        $pack = Pack::where('type', $request->type)->firstOrFail();
-        $dmInscription->idPack = $pack->idPack;
-        $offreActivite = offreActivite::where('idOffre', $request->idOffre)->first();
-        $ateliers = $request->Ateliers;
-        $prixTot = 0;
+            $pack = Pack::where('type', $request->type)->firstOrFail();
+            $dmInscription->idPack = $pack->idPack;
+            $offreActivite = OffreActivite::where('idOffre', $request->idOffre)->firstOrFail();
+            
+            $ateliers = $request->Ateliers;
+            $prixTot = 0;
 
-        if ($pack->type == 'PackAtelier') {
-            $this ->handlePackAtelier($dmInscription, $pack, $offreActivite, $Secenfants, $ateliers,$idTuteur);
+            if ($pack->type == 'PackAtelier') {
+                $this->handlePackAtelier($dmInscription, $pack, $offreActivite, $Secenfants, $ateliers, $idTuteur);
 
-        } elseif ($pack->type == 'PackEnfant' && $nbrEnfants > 2) {
-            $this ->handlePackEnfant($dmInscription, $pack, $offreActivite, $Secenfants, $idTuteur);
-        } else {
-            return response()->json(['error' => 'Le nombre d\'enfants doit être supérieur à 2 pour choisir le PackEnfant.'], 422);
-        }
-
-        DB::commit();
-        return response()->json(['message' => 'wa tahaaaaaa'], 201);
-    } catch (\Exception $e) {
-        DB::rollback();
-        return response()->json(['error' => 'Échec de la création de la demande. ' . $e->getMessage()], 422);
-    }
-}
-
-private function handlePackAtelier($dmInscription, $pack, $offreActivite, $Secenfants, $ateliers,$idTuteur)
-{
-    $i = 0;
-    $limite = $pack->limite;
-    $remise = $pack->remise;
-    $prixTot = 0;
-   
-    foreach ($Secenfants as $enfant) {
-        $prixT = [];
-        $prixHT = 0;
-        foreach ($ateliers as $AteliersData) {
-            $activite = Activite::where('idActivite', $AteliersData['idActivite'])->firstOrFail();
-            if (!$activite) {
-                return response()->json(['error' => 'Activite introuvable', 404]);
-            }
-            $idActivite = $activite->idActivite;
-
-            $prix = $offreActivite->where('idActivite', $idActivite)->firstOrFail();
-            $tarif = $prix->tarif;
-
-            $prixT[] = $tarif;
-            $prixHT += $tarif;
-        }
-        $prixTot = 0;
-        foreach ($prixT as $prixTA) {
-            $c = 0;
-            if ($c < $limite) {
-                $prixTot += $prixTA - ($c * $remise * $prixTA);
+            } elseif ($pack->type == 'PackEnfant' && $nbrEnfants > 2) {
+                $this->handlePackEnfant($dmInscription, $pack, $offreActivite, $Secenfants, $idTuteur);
             } else {
-                $prixTot += $prixTA;
+                return response()->json(['error' => 'Le nombre d\'enfants doit être supérieur à 2 pour choisir le PackEnfant.'], 422);
             }
-            $c++;
+
+            DB::commit();
+            return response()->json(['message' => 'Votre demande a été effectuée avec succès'], 201);
+        } catch (\Exception $e) {
+            DB::rollback();
+            return response()->json(['error' => 'Échec de la création de la demande. ' . $e->getMessage()], 422);
         }
-        $dmInscription->save();
-        $idoffre = $offreActivite->idOffre;
-        $idActivite = $offreActivite->idActivite;
-        $iddemande = $dmInscription->idDemande;
-
-        $dmInscription->enfantss()->attach($enfant['idEnfant'], [
-            'idDemande' => $iddemande,
-            'idTuteur' => $idTuteur,
-            'idOffre' => $idoffre,
-            'idActivite' => $idActivite,
-            'PixtotalRemise' => $prixTot,
-            'Prixbrute'=> $prixHT 
-        ]);
-        
-
     }
-}
 
-private function handlePackEnfant($dmInscription, $pack, $offreActivite, $Secenfants, $idTuteur)
-{
-    $remise = $pack->remise;
-    $limite = $pack->limite;
-    $enfantsSorted = collect($Secenfants)->sortBy(function ($enfant) use ($offreActivite) {
-        return $offreActivite->where('idOffre', $enfant['idOffre'])->count();
-    });
-
-    $childWithMinActivities = $enfantsSorted->first();
-    $enfantsSorted = $enfantsSorted->slice(1);
-    $prixTot = 0;
-    foreach ($enfantsSorted as $key => $enfant) {
-        $tarifs = $offreActivite->where('idOffre', $enfant['idOffre'])->pluck('tarif');
-
+    private function handlePackAtelier($dmInscription, $pack, $offreActivite, $Secenfants, $ateliers, $idTuteur)
+    {
         $i = 0;
-        foreach ($tarifs as $tarif) {
-            if ($i < $limite) {
-                $prixTot += $tarif - ($i * $remise * $tarif);
-            } else {
-                $prixTot += $tarif;
+        $limite = $pack->limite;
+        $remise = $pack->remise;
+        $prixTot = 0;
+        
+        foreach ($Secenfants as $enfant) {
+            $prixT = [];
+            $prixHT = 0;
+            $count = 0;
+           
+            foreach ($ateliers as $AteliersData) {
+                $activite = Activite::where('idActivite', $AteliersData['idActivite'])->firstOrFail();
+                $idActivite = $activite->idActivite;
+
+                $prix = $offreActivite->where('idActivite', $idActivite)->firstOrFail();
+                $tarif = $prix->tarif;
+
+                $prixT[] = $tarif;
+                $prixHT += $tarif;
+                $count++; // Calculer le nombre d'ateliers
             }
-            $i++;
+            
+
+            $prixTot = 0;
+            $c = 0;
+            foreach ($prixT as $prixTA) {
+                if ($c <= $count) {
+                    $prixTot += $prixTA - ($c * $remise * $prixTA);
+                } else {
+                    $prixTot += $prixTA;
+                }
+                $c++;
+            }
+            $dmInscription->save();
+            $idoffre = $offreActivite->idOffre;
+            $idActivite = $offreActivite->idActivite;
+            $iddemande = $dmInscription->idDemande;
+
+            $dmInscription->enfantss()->attach($enfant['idEnfant'], [
+                'idDemande' => $iddemande,
+                'idTuteur' => $idTuteur,
+                'idOffre' => $idoffre,
+                'idActivite' => $idActivite,
+                'PixtotalRemise' => $prixTot,
+                'Prixbrute' => $prixHT 
+            ]);
         }
     }
 
-    $dmInscription->save();
+    private function handlePackEnfant($dmInscription, $pack, $offreActivite, $Secenfants, $idTuteur)
+    {
+        $remise = $pack->remise;
+        $limite = $pack->limite;
+        $enfantsSorted = collect($Secenfants)->sortBy(function ($enfant) use ($offreActivite) {
+            return $offreActivite->where('idOffre', $enfant['idOffre'])->count();
+        });
 
-    foreach ($enfantsSorted as $key => $enfant) {
-        $idoffre = $enfant['idOffre'];
-        $idActivite = $offreActivite->where('idOffre', $idoffre)->first()->idActivite;
-        $iddemande = $dmInscription->idDemande;
+        $childWithMinActivities = $enfantsSorted->first();
+        $enfantsSorted = $enfantsSorted->slice(1);
+        $prixTot = 0;
+        foreach ($enfantsSorted as $key => $enfant) {
+            $tarifs = $offreActivite->where('idOffre', $enfant['idOffre'])->pluck('tarif');
 
-        $dmInscription->enfantss()->attach($enfant['idEnfant'], [
-            'idDemande' => $iddemande,
+            $i = 0;
+            foreach ($tarifs as $tarif) {
+                if ($i < $limite) {
+                    $prixTot += $tarif - ($i * $remise * $tarif);
+                } else {
+                    $prixTot += $tarif;
+                }
+                $i++;
+            }
+        }
+
+        $dmInscription->save();
+
+        foreach ($enfantsSorted as $key => $enfant) {
+            $idoffre = $enfant['idOffre'];
+            $idActivite = $offreActivite->where('idOffre', $idoffre)->first()->idActivite;
+            $iddemande = $dmInscription->idDemande;
+
+            $dmInscription->enfantss()->attach($enfant['idEnfant'], [
+                'idDemande' => $iddemande,
+                'idTuteur' => $idTuteur,
+                'idOffre' => $idoffre,
+                'idActivite' => $idActivite,
+                'PixtotalRemise' => $prixTot,
+                'Prixbrute' => 0
+            ]);
+        }
+
+        $childOffre = $childWithMinActivities['idOffre'];
+        $childActivite = $offreActivite->where('idOffre', $childOffre)->first()->idActivite;
+        $dmInscription->enfantss()->attach($childWithMinActivities['idEnfant'], [
+            'idDemande' => $dmInscription->idDemande,
             'idTuteur' => $idTuteur,
-            'idOffre' => $idoffre,
-            'idActivite' => $idActivite,
-            'PixtotalRemise' => $prixTot,
-            'Prixbrute' =>0
+            'idOffre' => $childOffre,
+            'idActivite' => $childActivite,
+            'PixtotalRemise' => 0,
+            'Prixbrute' => 0
         ]);
     }
- 
-    $childOffre = $childWithMinActivities['idOffre'];
-    $childActivite = $offreActivite->where('idOffre', $childOffre)->first()->idActivite;
-    $dmInscription->enfantss()->attach($childWithMinActivities['idEnfant'], [
-        'idDemande' => $dmInscription->idDemande,
-        'idTuteur' => $idTuteur,
-        'idOffre' => $childOffre,
-        'idActivite' => $childActivite,
-        'PixtotalRemise' => 0,
-        'Prixbrute' => 0
-    ]);
 }
-    
-   
-} 
