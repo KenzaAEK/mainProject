3f880401c5c3c4a19ec73a583a4c30c6774d504e
imad <imad@dns.localdomain>
2024-06-20 21:37:51 +0100

    corriger les erreur

--- server/app/Providers/AuthServiceProvider.php
+++ server/app/Providers/AuthServiceProvider.php
@@ -2,13 +2,9 @@
 
 namespace App\Providers;
 
-
 use Illuminate\Auth\Notifications\VerifyEmail;
-
 use App\Models\Devis;
 use App\Models\Facture;
-use App\Models\Notification;
-
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Support\Facades\Gate;
@@ -33,25 +29,23 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-// from verify email
+        // from verify email
         VerifyEmail::toMailUsing(function ($notifiable, $url) {
             $spaUrl = "http://spa.test?email_verify_url=".$url;
 
             return (new MailMessage)
-            ->subject('Vérifier l\'adresse e-mail')
-            ->line('Cliquez sur le bouton ci-dessous pour vérifier votre adresse e-mail.')
-            ->action('Vérifier l\'adresse e-mail', $spaUrl);
-            
+                ->subject('Vérifier l\'adresse e-mail')
+                ->line('Cliquez sur le bouton ci-dessous pour vérifier votre adresse e-mail.')
+                ->action('Vérifier l\'adresse e-mail', $spaUrl);
+        }); // <- Ajout de cette accolade fermante
 
         Gate::define('download-facture', function ($user, $factureId) {
             $facture = Facture::findOrFail($factureId);
             return $facture->notification->idUser === $user->idUser;
         });
-        
-        
+
         Gate::define('manage-devis', function ($user, Devis $devis) {
             return $user->idUser === $devis->demandeInscription->tuteur->idUser;
-
         });
     }
 }
